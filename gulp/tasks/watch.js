d51f27ccf66e7a5edc6282932ff3f326b5ae2b2f
Sergio Gutiérrez <sergio_gutierrez_1@hotmail.com>
2018-09-04 11:13:12 +0200

    Completed desktop and mobile header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,9 +18,17 @@ gulp.task('watch', function(){
     watch('./app/assets/styles/**/*.css', function(){
         gulp.start('cssInject');
     })
+
+    watch('./app/assets/scripts/**/*.js', function(){
+        gulp.start('scriptsRefresh');
+    })
 })
 
 gulp.task('cssInject', ['styles'], function(){ //en el medio especificamos tareas que se ejectuarán antes de esta
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
+})
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+    browserSync.reload();
 })
